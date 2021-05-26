e8b53d2af3199ec807d35bb0c63958cbb54cf796
Danilo Solórzano <danilo_solorzano06@hotmail.com>
2021-05-26 16:40:59 -0600

    update navbar

--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [NavbarComponent, FooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [NavbarComponent, FooterComponent],
 })
 export class ShareModule {}
