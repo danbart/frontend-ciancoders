7c61bde4c62a19d3ec2f7364b7ed1a612217fc69
Danilo Solórzano <danilo_solorzano06@hotmail.com>
2021-05-27 00:04:01 -0600

    update navbar login url

--- src/app/share/navbar/navbar.component.ts
+++ src/app/share/navbar/navbar.component.ts
@@ -1,17 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
 import { AUTH, DASHBOARD, LOGIN, REGISTER } from 'src/app/common/constants';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnChanges {
   dashboard: string = DASHBOARD;
   login = `${AUTH}/${LOGIN}`;
   register = `${AUTH}/${REGISTER}`;
+  isLoggedIn = false;
 
-  constructor() {}
+  constructor(public authService: AuthService) {}
 
   ngOnInit(): void {}
+
+  ngOnChanges() {
+    this.isLoggedIn = this.authService.isLoggedIn();
+  }
 }
