fd591fc95d50e0cd554ca60ee84cfd6e8f9c007a
Danilo Solórzano <danilo_solorzano06@hotmail.com>
2021-05-27 17:27:47 -0600

    update add products

--- src/app/components/dashboard/product/add/add.component.ts
+++ src/app/components/dashboard/product/add/add.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DASHBOARD } from 'src/app/common/constants';
+import { ProductService } from 'src/app/services/product.service';
+import Swal from 'sweetalert2';
+import { CategoryInterface } from '../../../../common/interfaces/category.interface';
 import { ProductInterface } from '../../../../common/interfaces/product.interface';
 
 @Component({
@@ -9,8 +14,13 @@ import { ProductInterface } from '../../../../common/interfaces/product.interfac
 })
 export class AddComponent implements OnInit {
   product: ProductInterface;
+  category: CategoryInterface;
   productForm: FormGroup;
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    public productService: ProductService,
+    private router: Router
+  ) {
     this.productForm = this.fb.group({
       name: [''],
       description: [''],
@@ -20,9 +30,37 @@ export class AddComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.productService.getCategory().then((res) => (this.category = res));
+  }
 
   saveProduct() {
-    console.log(this.productForm.value);
+    if (!this.productForm.valid) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Invalid Form',
+      });
+    } else {
+      this.productService
+        .postProduct(this.productForm.value)
+        .then((res) => {
+          Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: res.message,
+            timer: 1500,
+          });
+          this.productForm.reset();
+          this.router.navigate([`/${DASHBOARD}`]);
+        })
+        .catch((err) => {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: JSON.stringify(err.error),
+          });
+        });
+    }
   }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { API, CATEGORY, PRODUCT } from '../common/constants';
-import { CategoryInterface } from '../common/interfaces/category.interface';
 import { ProductInterface } from '../common/interfaces/product.interface';
 
 @Injectable({
@@ -11,47 +10,21 @@ export class ProductService {
   productUrl = `${API}/${PRODUCT}`;
   categoryUrl = `${API}/${CATEGORY}`;
 
-  private products: ProductInterface[] = [];
-  private categories: CategoryInterface[] = [];
-
   constructor(private http: HttpClient) {}
 
-  getProduct(): Promise<ProductInterface[]> {
-    if (this.products.length > 0) {
-      return Promise.resolve(this.products);
-    }
-    return new Promise((resolve) => {
-      this.http
-        .get(this.productUrl)
-        .subscribe((products: ProductInterface[]) => {
-          this.products = products;
-          resolve(products);
-        });
-    });
+  getProduct(): Promise<any> {
+    return this.http.get(this.productUrl).toPromise();
   }
 
-  getCategory(): Promise<CategoryInterface[]> {
-    if (this.categories.length > 0) {
-      return Promise.resolve(this.categories);
-    }
-    return new Promise((resolve) => {
-      this.http
-        .get(this.categoryUrl)
-        .subscribe((categories: CategoryInterface[]) => {
-          this.categories = categories;
-          resolve(categories);
-        });
-    });
+  getCategory(): Promise<any> {
+    return this.http.get(this.categoryUrl).toPromise();
   }
 
-  getCategoryId(id: number): Promise<CategoryInterface> {
-    if (this.categories.length > 0) {
-      const category = this.categories.find((c) => c.id === id);
-      return Promise.resolve(category);
-    }
-    return this.getCategory().then((categories) => {
-      const category = categories.find((c) => c.id === id);
-      return Promise.resolve(category);
-    });
+  getCategoryId(id: number): Promise<any> {
+    return this.http.get(this.categoryUrl + '/' + id).toPromise();
+  }
+
+  postProduct(product: ProductInterface): Promise<any> {
+    return this.http.post(this.productUrl, product).toPromise();
   }
 }
