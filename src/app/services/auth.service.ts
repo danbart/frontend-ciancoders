6e73556956a3da9e29870ab12371ffa4c234608c
Danilo Solórzano <danilo_solorzano06@hotmail.com>
2021-05-27 00:03:49 -0600

    updates services

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,11 +1,44 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { API } from '../common/constants';
+import { Router } from '@angular/router';
+import {
+  ACCESS_TOKEN,
+  API,
+  AUTH,
+  LOGIN,
+  REGISTER,
+  USER,
+} from '../common/constants';
+import { UserInterface } from '../common/interfaces/user.interface';
 
 const baseUrl = `${API}`;
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private httpClient: HttpClient) {}
+  loginUrl = `${API}/${AUTH}/${LOGIN}`;
+  registerUrl = `${API}/${AUTH}/${REGISTER}`;
+  headers = new HttpHeaders().set('Content-Type', 'application/json');
+  constructor(private http: HttpClient, private router: Router) {}
+
+  signIn(user: UserInterface): Promise<any> {
+    return this.http.post(this.loginUrl, user).toPromise();
+  }
+
+  signUp(user: UserInterface): Promise<any> {
+    return this.http.post(this.registerUrl, user).toPromise();
+  }
+
+  isLoggedIn(): boolean {
+    let authToken = localStorage.getItem(ACCESS_TOKEN);
+    return authToken !== null ? true : false;
+  }
+
+  doLogout() {
+    let removeToken = localStorage.removeItem(ACCESS_TOKEN);
+    let removeUserInfo = localStorage.removeItem(USER);
+    if (removeToken == null && removeUserInfo == null) {
+      this.router.navigate([`${AUTH}/${LOGIN}`]);
+    }
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,15 +1,57 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { API, PRODUCT } from '../common/constants';
+import { API, CATEGORY, PRODUCT } from '../common/constants';
+import { CategoryInterface } from '../common/interfaces/category.interface';
+import { ProductInterface } from '../common/interfaces/product.interface';
 
-const baseUrl = `${API}/${PRODUCT}`;
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  constructor(private httpClient: HttpClient) {}
+  productUrl = `${API}/${PRODUCT}`;
+  categoryUrl = `${API}/${CATEGORY}`;
 
-  getProduct() {
-    return this.httpClient.get(baseUrl);
+  private products: ProductInterface[] = [];
+  private categories: CategoryInterface[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  getProduct(): Promise<ProductInterface[]> {
+    if (this.products.length > 0) {
+      return Promise.resolve(this.products);
+    }
+    return new Promise((resolve) => {
+      this.http
+        .get(this.productUrl)
+        .subscribe((products: ProductInterface[]) => {
+          this.products = products;
+          resolve(products);
+        });
+    });
+  }
+
+  getCategory(): Promise<CategoryInterface[]> {
+    if (this.categories.length > 0) {
+      return Promise.resolve(this.categories);
+    }
+    return new Promise((resolve) => {
+      this.http
+        .get(this.categoryUrl)
+        .subscribe((categories: CategoryInterface[]) => {
+          this.categories = categories;
+          resolve(categories);
+        });
+    });
+  }
+
+  getCategoryId(id: number): Promise<CategoryInterface> {
+    if (this.categories.length > 0) {
+      const category = this.categories.find((c) => c.id === id);
+      return Promise.resolve(category);
+    }
+    return this.getCategory().then((categories) => {
+      const category = categories.find((c) => c.id === id);
+      return Promise.resolve(category);
+    });
   }
 }
