765a3913e30642e29b0b1b8055e2ba99309494a7
Danilo Solórzano <danilo_solorzano06@hotmail.com>
2021-05-27 11:22:04 -0600

    created profile and edit profile

--- src/app/components/auth/auth.module.ts
+++ src/app/components/auth/auth.module.ts
@@ -2,18 +2,32 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
-import { LOGIN } from 'src/app/common/constants';
-import { REGISTER } from '../../common/constants';
+import { LOGIN, PROFILE, REGISTER } from 'src/app/common/constants';
+import { EDIT_PROFILE } from '../../common/constants';
+import { AuthGuard } from './auth.guard';
+import { EditUserComponent } from './edit-user/edit-user.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   { path: LOGIN, component: LoginComponent },
   { path: REGISTER, component: RegisterComponent },
+  { path: PROFILE, component: UserComponent, canActivate: [AuthGuard] },
+  {
+    path: EDIT_PROFILE + '/:id',
+    component: EditUserComponent,
+    canActivate: [AuthGuard],
+  },
 ];
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent],
+  declarations: [
+    LoginComponent,
+    RegisterComponent,
+    UserComponent,
+    EditUserComponent,
+  ],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
---
+++ src/app/components/auth/user/user.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2';
+import { EDIT_PROFILE } from '../../../common/constants';
+import { UserInterface } from '../../../common/interfaces/user.interface';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css'],
+})
+export class UserComponent implements OnInit {
+  user: UserInterface;
+  editProfile: string = EDIT_PROFILE;
+  constructor(public authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.authService
+      .getUserProfile()
+      .then((user) => (this.user = user))
+      .catch((err) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: err.error.error,
+        });
+      });
+  }
+}
