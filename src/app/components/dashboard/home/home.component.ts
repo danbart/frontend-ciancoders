68884d433a10462f93e29f9963f12365123e1b0f
Danilo Solórzano <danilo_solorzano06@hotmail.com>
2021-05-27 00:03:05 -0600

    update dashboards route

--- src/app/components/dashboard/home/home.component.ts
+++ src/app/components/dashboard/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Product } from 'src/app/common/interfaces/product';
+import { CategoryInterface } from 'src/app/common/interfaces/category.interface';
+import { ProductInterface } from 'src/app/common/interfaces/product.interface';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +9,18 @@ import { Product } from 'src/app/common/interfaces/product';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  product: Product;
+  product: ProductInterface[] = [];
+  category: CategoryInterface;
   lodingPrdoduct = false;
-  constructor() {}
-  // public productService: ProductService
+  constructor(public productService: ProductService) {}
+
   ngOnInit(): void {
-    // this.productService.getProduct().subscribe(console.log);
+    this.productService
+      .getProduct()
+      .then((product) => (this.product = product));
+  }
+
+  searchCategory(id: number) {
+    return this.productService.getCategoryId(id).then((c) => c.category);
   }
 }
